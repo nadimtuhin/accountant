c5c5cfc0592cb2ca1fa5bf951c408790e79f5825
Nadim Tuhin <nadimtuhin@gmail.com>
2017-03-30 21:47:59 +0600

    added more terms

---
+++ src/constants/terms.js
@@ -0,0 +1,8 @@
+export const LOAN = 'LOAN';
+export const DEBT = 'DEBT';
+export const EXPENSE = 'EXPENSE';
+export const INCOME = 'INCOME';
+export const REPAYMENT = 'REPAYMENT';
+export const DEBT_COLLECTION = 'DEBT_COLLECTION';
+export const LDRD = 'LDRD';
+export const SALARY = 'SALARY';
