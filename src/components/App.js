98f9efe847616830a775acab839fbd38334ef522
Nadim Tuhin <nadimtuhin@gmail.com>
2017-04-01 02:34:12 +0600

    can switch months now

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
 import Transactions from './Transactions';
+import { monthsSelector } from '../selectors/index';
+
 
 class App extends Component {
+  static propTypes = {
+    months: PropTypes.array.isRequired
+  };
+
+  state = { month: 'year/month' };
+
+  handleMonthChange = (event) => this.setState({ month: event.target.value });
+
   render() {
     return (
       <div className="App">
-        <Transactions month={'year/month'} />
+        <select onChange={this.handleMonthChange} value={this.state.month}>
+          { this.props.months.map(month => <option value={month.id} key={month.id}>{month.id}</option>)}
+        </select>
+        <Transactions month={this.state.month} />
       </div>
     );
   }
 }
 
-export default App;
+export default connect(state => ({
+  months: monthsSelector(state)
+}))(App);
