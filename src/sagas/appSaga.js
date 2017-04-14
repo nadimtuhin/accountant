e38cbbe1ff5a5a8947259357cab6b9b4b912d1fe
Nadim Tuhin <nadimtuhin@gmail.com>
2017-04-15 03:22:52 +0600

    when a new transaction is created change month

--- src/sagas/appSaga.js
+++ src/sagas/appSaga.js
@@ -1,5 +1,20 @@
+import { takeEvery, put } from 'redux-saga/effects';
+import { ADD_TRANSACTION } from '../constants/actionTypes';
+import getMonthName from '../reducers/lib/getMonthName';
+import { changeMonth } from '../actions/settingsActionCreators';
+
+export function* changeMonthAsync({ payload }) {
+  const { transaction } = payload;
+  const month = getMonthName(transaction.date);
+  yield put(changeMonth(month));
+}
+
+export function* watchTransactions() {
+  yield takeEvery(ADD_TRANSACTION, changeMonthAsync);
+}
+
 export default function* appSaga() {
   yield [
-
+    watchTransactions()
   ];
 }
